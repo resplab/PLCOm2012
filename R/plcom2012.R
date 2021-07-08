ab625a0dcbba3de067f597437961227ca9b87483
William Xu <yihangxu001@hotmail.com>
2021-07-08 16:51:45 -0700

    Add roxygen2 documentation

--- R/plcom2012.R
+++ R/plcom2012.R
@@ -1,10 +1,28 @@
-#' PLCO_M2012 risk prediction model
+#' PLCOm2012 risk prediction model
 #'
-#' @return
+#' @param age a vector of patient's age
+#' @param race categorical variable of patient's race or ethnic group (White, Black, Hispanic,
+#' Asian, American Indian, Alaskan Native, Native Hawaiian, Pacific Islander)
+#' @param education education was measured in six ordinal levels: less than high-school graduate (level 1),
+#' high-school graduate (level 2), some training after high school (level 3), some college (level 4),
+#' college graduate (level 5), and postgraduate or professional degree (level 6)
+#' @param bmi a vector of patient's body mass index, per 1 unit of increase
+#' @param copd binary variable of chronic obstructive pulmonary disease (yes as 1 or no as 0)
+#' @param cancer_hist binary variable of patient's cancer history (yes as 1 or no as 0)
+#' @param family_hist_lung_cancer binary variable of patient's family history of lung cancer (yes as 1 or no as 0)
+#' @param smoking_status binary variable of patient's smoking status (current as 1 or former as 0)
+#' @param smoking_intensity a vector of the number cigarettes patient smokes per day
+#' @param duration_smoking a vector of patient's duration of smoking, per 1-yr increase
+#' @param smoking_quit_time a vector of patient's smoking quit time, per 1-yr increase
+#'
+#' @return prob patient’s 6-year probability of lung-cancer
 #' @export
 #'
 #' @examples
-
+#' plcom2012(age=57, race="Black", education=6, bmi=24, copd=1,
+#' cancer_hist=1, family_hist_lung_cancer=1, smoking_status=1,
+#' smoking_intensity=3, duration_smoking=2, smoking_quit_time=3
+#' )
 plcom2012 <-
   function(age,
            race,
