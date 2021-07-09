71b87c8e4011daf8f5ebe31850dd69f790723979
yzr96 <eric.yu@usask.ca>
2021-07-09 16:26:15 -0700

    fix the function

--- R/plcom2012.R
+++ R/plcom2012.R
@@ -35,7 +35,6 @@ plcom2012 <-
            smoking_intensity,
            duration_smoking,
            smoking_quit_time) {
-
     if (typeof(age) != 'double') {
       stop('Your input data for age should be a number.')
     }
@@ -43,12 +42,23 @@ plcom2012 <-
       stop('Your input data for age should be greater than 0 and less than 100.')
     }
 
-    %notin% <- Negate(%in%)
-    if (race %notin% c('White', 'American Indian', 'Alaskan Native', 'Black', 'Hispanic', 'Asian', 'Native Hawaiian', 'Pacific Islander')) {
-      stop('Your input data for race should come from White, American Indian, Alaskan Native, Black, Hispanic, Asian, Native Hawaiian, Pacific Islander.')
+    `%!in%` <- Negate(`%in%`)
+    if (race %!in% c(
+      'White',
+      'American Indian',
+      'Alaskan Native',
+      'Black',
+      'Hispanic',
+      'Asian',
+      'Native Hawaiian',
+      'Pacific Islander'
+    )) {
+      stop(
+        'Your input data for race should come from White, American Indian, Alaskan Native, Black, Hispanic, Asian, Native Hawaiian, Pacific Islander.'
+      )
     }
 
-    if (education %notin% c(1, 2, 3, 4, 5, 6)) {
+    if (education %!in% c(1, 2, 3, 4, 5, 6)) {
       stop('Your input data for education should be 1 or 2 or 3 or 4 or 5 or 6.')
     }
 
@@ -60,19 +70,23 @@ plcom2012 <-
       stop('Your input data for bmi should be a positive number.')
     }
 
-    if (copd %notin% c(1, 0)) {
+    if (copd %!in% c(1, 0)) {
       stop('Your input data for copd should be either 1 or 0. 1 as yes and 0 as no.')
     }
 
-    if (cancer_hist %notin% c(1, 0)) {
-      stop('Your input data for personal history of cancer should be either 1 or 0. 1 as yes and 0 as no.')
+    if (cancer_hist %!in% c(1, 0)) {
+      stop(
+        'Your input data for personal history of cancer should be either 1 or 0. 1 as yes and 0 as no.'
+      )
     }
 
-    if (family_hist_lung_cancer %notin% c(1, 0)) {
-      stop('Your input data for family history of lung cancer should be either 1 or 0. 1 as yes and 0 as no.')
+    if (family_hist_lung_cancer %!in% c(1, 0)) {
+      stop(
+        'Your input data for family history of lung cancer should be either 1 or 0. 1 as yes and 0 as no.'
+      )
     }
 
-    if (smoking_status %notin% c(1, 0)) {
+    if (smoking_status %!in% c(1, 0)) {
       stop('Your input data for smoking status should be either 1 or 0. 1 as current and 0 as former.')
     }
 
@@ -92,42 +106,52 @@ plcom2012 <-
     if (race == 'White' |
         race == 'American Indian' | race == 'Alaskan Native') {
       model <-
-        0.0778868 * age - 0.0812744 * education - 0.0274194 * bmi + 0.3553063 *
+        0.0778868 * (age - 62) - 0.0812744 * (education - 4) - 0.0274194 * (bmi -
+                                                                              27) + 0.3553063 *
         copd + 0.4589971 * cancer_hist + 0.587185 * family_hist_lung_cancer + 0.2597431 *
-        smoking_status - 1.822606 * smoking_intensity + 0.0317321 * duration_smoking -
-        0.0308572 * smoking_quit_time - 4.532506
+        smoking_status - 1.822606 * ((smoking_intensity / 10) ^ (-1) - 0.4021541613) + 0.0317321 * (duration_smoking -
+                                                                                                      27) -
+        0.0308572 * (smoking_quit_time - 10) - 4.532506
     }
 
     if (race == 'Black') {
       model <-
-        0.0778868 * age - 0.0812744 * education - 0.0274194 * bmi + 0.3553063 *
+        0.0778868 * (age - 62) - 0.0812744 * (education - 4) - 0.0274194 * (bmi -
+                                                                              27) + 0.3553063 *
         copd + 0.4589971 * cancer_hist + 0.587185 * family_hist_lung_cancer + 0.2597431 *
-        smoking_status - 1.822606 * smoking_intensity + 0.0317321 * duration_smoking -
-        0.0308572 * smoking_quit_time - 4.532506 + 0.3944778
+        smoking_status - 1.822606 * ((smoking_intensity / 10) ^ (-1) - 0.4021541613) + 0.0317321 * (duration_smoking -
+                                                                                                      27) -
+        0.0308572 * (smoking_quit_time - 10) - 4.532506 + 0.3944778
     }
 
     if (race == 'Hispanic') {
       model <-
-        0.0778868 * age - 0.0812744 * education - 0.0274194 * bmi + 0.3553063 *
+        0.0778868 * (age - 62) - 0.0812744 * (education - 4) - 0.0274194 * (bmi -
+                                                                              27) + 0.3553063 *
         copd + 0.4589971 * cancer_hist + 0.587185 * family_hist_lung_cancer + 0.2597431 *
-        smoking_status - 1.822606 * smoking_intensity + 0.0317321 * duration_smoking -
-        0.0308572 * smoking_quit_time - 4.532506 - 0.7434744
+        smoking_status - 1.822606 * ((smoking_intensity / 10) ^ (-1) - 0.4021541613) + 0.0317321 * (duration_smoking -
+                                                                                                      27) -
+        0.0308572 * (smoking_quit_time - 10) - 4.532506 - 0.7434744
     }
 
     if (race == 'Asian') {
       model <-
-        0.0778868 * age - 0.0812744 * education - 0.0274194 * bmi + 0.3553063 *
+        0.0778868 * (age - 62) - 0.0812744 * (education - 4) - 0.0274194 * (bmi -
+                                                                              27) + 0.3553063 *
         copd + 0.4589971 * cancer_hist + 0.587185 * family_hist_lung_cancer + 0.2597431 *
-        smoking_status - 1.822606 * smoking_intensity + 0.0317321 * duration_smoking -
-        0.0308572 * smoking_quit_time - 4.532506 − 0.466585
+        smoking_status - 1.822606 * ((smoking_intensity / 10) ^ (-1) - 0.4021541613) + 0.0317321 * (duration_smoking -
+                                                                                                      27) -
+        0.0308572 * (smoking_quit_time - 10) - 4.532506 - 0.466585
     }
 
     if (race == 'Native Hawaiian' | race == 'Pacific Islander') {
       model <-
-        0.0778868 * age - 0.0812744 * education - 0.0274194 * bmi + 0.3553063 *
+        0.0778868 * (age - 62) - 0.0812744 * (education - 4) - 0.0274194 * (bmi -
+                                                                              27) + 0.3553063 *
         copd + 0.4589971 * cancer_hist + 0.587185 * family_hist_lung_cancer + 0.2597431 *
-        smoking_status - 1.822606 * smoking_intensity + 0.0317321 * duration_smoking -
-        0.0308572 * smoking_quit_time - 4.532506 + 1.027152
+        smoking_status - 1.822606 * ((smoking_intensity / 10) ^ (-1) - 0.4021541613) + 0.0317321 * (duration_smoking -
+                                                                                                      27) -
+        0.0308572 * (smoking_quit_time - 10) - 4.532506 + 1.027152
     }
 
     prob <- exp(model) / (1 + exp(model))
@@ -135,3 +159,6 @@ plcom2012 <-
     return(prob)
 
   }
+
+plcom2012(age=57, race="Black", education=6, bmi=24, copd=1, cancer_hist=1, family_hist_lung_cancer=1, smoking_status=1,
+          smoking_intensity=3, duration_smoking=2, smoking_quit_time=3)
